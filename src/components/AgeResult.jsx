8bf24a443d20333b4e34c88a6bffb7c576f64d85
kasimurkmez <kasimurkmez47@gmail.com>
2023-06-23 22:42:20 +0300

    updated

--- src/components/AgeResult.jsx
+++ src/components/AgeResult.jsx
@@ -1,9 +1,21 @@
-const AgeResult = () => {
+import PropTypes from "prop-types";
+
+const AgeResult = ({ age }) => {
   return (
-    <div>
-      <h2>Age Result</h2>
-    </div>
+    <h4 className="mt-10 text-4xl font-normal leading-normal  mb-2 text-pink-800">
+      You are <span className="text-lime-800">{age.years}</span> years,
+      <span className="text-cyan-800">{age.months}</span> months, and{" "}
+      <span className="text-yellow-800">{age.days}</span> days old.
+    </h4>
   );
+};
+
+AgeResult.propTypes = {
+  age: PropTypes.shape({
+    years: PropTypes.number.isRequired,
+    months: PropTypes.number.isRequired,
+    days: PropTypes.number.isRequired,
+  }),
 };
 
 export default AgeResult;
