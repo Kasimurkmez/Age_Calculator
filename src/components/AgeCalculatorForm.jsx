e3312efd35925c947e990de3d2b39890cf82211e
kasimurkmez <kasimurkmez47@gmail.com>
2023-06-23 22:18:20 +0300

    updated

--- src/components/AgeCalculatorForm.jsx
+++ src/components/AgeCalculatorForm.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-function AgeCalculatorForm() {
+function AgeCalculatorForm({ calculateAge }) {
+  const [birthDate, setBirthDate] = useState("");
+
+  console.log(birthDate);
+  const handleChangeDate = (event) => {
+    setBirthDate(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    calculateAge(birthDate);
+  };
   return (
-    <div>
-      <input type="date" />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input value={birthDate} type="date" onChange={handleChangeDate} />
+      <button disabled={!birthDate} type="submit">
+        calculate Age
+      </button>
+    </form>
   );
 }
 
+AgeCalculatorForm.propTypes = {
+  calculateAge: PropTypes.func.isRequired,
+};
 export default AgeCalculatorForm;
